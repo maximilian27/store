0e091d7b753e3728ff3891552bc2859ae0e20112
Maximilian Both <32038674+maximilian27@users.noreply.github.com>
2018-01-28 19:48:53 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,40 +7,40 @@ function myMap() {
 	marker.setMap(map);
 }
 
+(function() {
+	var totalSum = 0.00;
+	var i=0;
 
-var totalSum = 0.00;
-var i=0;
-
-$('.add-to-cart').click(function() {
-	i++;
-	$('.badge-notify').removeClass('hidden');
-	$('.badge-notify').text(i);
-	$('.cart-link').attr('title', '');
-	
-		 // add items to cart list.. 
-		var itemName = $(this).closest('.panel').find('.item-name').text();
-		var price = $(this).closest('.panel').find('.item-price').text();
-			var cartItem = $('<li class="list-item"></li>');
-			cartItem.html('<a href="#section1">' + itemName + ' / ' + '<span class="elem-price">' + price + '</span>' + '</a>' + '<button type="button" class="delete-item">X</button>');
-			$('.cart-item').append(cartItem);
-				totalSum += Number(price);
-				$('.total-cost').html('Total price: ' + totalSum.toFixed(2));
-				
-		// delete items from cart
-		var totalSum2 = totalSum;
-		var j=i;
-		$('.delete-item').click(function() {
-			j--;
-			$('.badge-notify').text(j);
-			$(this).closest('.list-item').remove();
-			var price2 = $(this).closest('.list-item').find('.elem-price').text();
-			totalSum2 -= Number(price2);
-			$('.total-cost').html('Total price: ' + totalSum2.toFixed(2));
-				i = j;
-				totalSum = totalSum2;
-		});
-});	
-
+	$('.add-to-cart').click(function() {
+		i++;
+		$('.badge-notify').removeClass('hidden');
+		$('.badge-notify').text(i);
+		$('.cart-link').attr('title', '');
+		
+			 // add items to cart list.. 
+			var itemName = $(this).closest('.panel').find('.item-name').text();
+			var price = $(this).closest('.panel').find('.item-price').text();
+				var cartItem = $('<li class="list-item"></li>');
+				cartItem.html('<a href="#section1">' + itemName + ' / ' + '<span class="elem-price">' + price + '</span>' + '</a>' + '<button type="button" class="delete-item">X</button>');
+				$('.cart-item').append(cartItem);
+					totalSum += Number(price);
+					$('.total-cost').html('Total price: ' + totalSum.toFixed(2));
+					
+			// delete items from cart
+			var totalSum2 = totalSum;
+			var j=i;
+			$('.delete-item').click(function() {
+				j--;
+				$('.badge-notify').text(j);
+				$(this).closest('.list-item').remove();
+				var price2 = $(this).closest('.list-item').find('.elem-price').text();
+				totalSum2 -= Number(price2);
+				$('.total-cost').html('Total price: ' + totalSum2.toFixed(2));
+					i = j;
+					totalSum = totalSum2;
+			});
+	});	
+})();  
 
 
 $('#search-button').click(function() {
